0c35aeb0545987c5273fd88d203630da09129538
blancohr19 <62089624+blancohr19@users.noreply.github.com>
2021-09-16 08:21:04 -0500

    Actualización Proyecto

---
+++ HospiEnCasaMascotas.App.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HospiEnCasaMascotas.App.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ HospiEnCasaMascotas.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HospiEnCasaMascotas.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
