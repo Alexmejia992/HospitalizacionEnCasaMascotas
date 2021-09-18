7e4fbe33f4ed41ddf8631cc6ca7508f858ffa70e
henry2080 <rctbpublicidad@gmail.com>
2021-09-17 23:42:22 -0500

    Create Veterinario.cs

---
+++ HospiEnCasaMascotas.App.Dominio/Entidades/Veterinario.cs
@@ -0,0 +1,14 @@
+namespace HospiEnCasa.App.Dominio
+{
+     public class Veterinario
+    {
+        static void Main(string[] args)
+
+        {
+        public string nombre {get;set;}
+        public string numPermiso {get;set;}
+        public string numtelefono {get;set;}
+
+        }
+    }
+}
