11a5006544e91e81e6b55f32c60410db66f48450
Alexmejia992 <alexmejia992@gmail.com>
2021-09-18 00:25:54 -0500

    add generoMasctoa

--- HospiEnCasaMascotas.App.Dominio/Entidades/Mascota.cs
+++ HospiEnCasaMascotas.App.Dominio/Entidades/Mascota.cs
@@ -5,10 +5,10 @@ namespace HospiEnCasaMascotas.App.Dominio
     public class Mascota
     {
         public string IdMascota { get;set; }
-
         public string NombreMascota { get;set; }
         public string Especie { get;set; }
         public string Raza{ get;set; }
+        public GeneroMascota generoMascota { get;set; }
         public string Edad { get;set; }
     }
 }
