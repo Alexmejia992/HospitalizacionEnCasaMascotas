20db2390532053174ab0d657df645268d78d7908
alexmejia992 <71393859+Alexmejia992@users.noreply.github.com>
2021-09-18 00:12:33 -0500

    Update Mascota.cs

--- HospiEnCasaMascotas.App.Dominio/Entidades/Mascota.cs
+++ HospiEnCasaMascotas.App.Dominio/Entidades/Mascota.cs
@@ -5,7 +5,6 @@ namespace HospiEnCasaMascotas.App.Dominio
     public class Mascota
     {
         public string IdMascota { get;set; }
-
         public string NombreMascota { get;set; }
         public string Especie { get;set; }
         public string Raza{ get;set; }
