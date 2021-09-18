d5ab756256fb6eeae222c3bd1e6f58b62d064985
Andreslis19 <gaml_16@hotmail.com>
2021-09-17 23:10:15 -0500

    Update SugerenciaCuidado.cs
    modificacion entidad sugerencia cuidado ,creacion nuevas propiedades

--- HospiEnCasaMascotas.App.Dominio/Entidades/SugerenciaCuidado.cs
+++ HospiEnCasaMascotas.App.Dominio/Entidades/SugerenciaCuidado.cs
@@ -9,5 +9,11 @@ namespace HospiEnCasaMascotas.App.Dominio
         public DateTime FechaHora  { get; set; }
 
         public string Descripcion {get;set;}
+
+        public string alimentacion {get;set;}
+
+        public string Limpieza_heridas{get;set;}
+
+        public string Ejercicios{get;set;}
     }
 }
