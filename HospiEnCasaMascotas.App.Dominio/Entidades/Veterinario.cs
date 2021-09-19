7d5f82b4b64ff7937657cec4c927bb643cfca2cc
henry2080 <rctbpublicidad@gmail.com>
2021-09-18 20:05:31 -0500

    Update Veterinario.cs

--- HospiEnCasaMascotas.App.Dominio/Entidades/Veterinario.cs
+++ HospiEnCasaMascotas.App.Dominio/Entidades/Veterinario.cs
@@ -5,14 +5,9 @@ namespace HospiEnCasaMascotas.App.Dominio
         
 
         {
-<<<<<<< HEAD
-        public string numPermiso {get;set;}
-        public string especialidad {get;set;}
-=======
         public int NumPermiso {get;set;}
         public string especialidad {get;set;}
         
->>>>>>> 476cb2d728a9f5d407cf1a88a014a6dafe483028
 
         }
     }
