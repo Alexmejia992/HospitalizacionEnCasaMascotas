acf600a44357ae0040f5fa150147a373d00e8270
Andreslis19 <gaml_16@hotmail.com>
2021-10-04 23:37:30 -0500

    metodo CRUD capa consola
    verificacion metodo crud desde la capa consola con el repositorio mascota

--- HospiEnCasaMascotas.App.Consola/Program.cs
+++ HospiEnCasaMascotas.App.Consola/Program.cs
@@ -11,7 +11,19 @@ namespace HospiEnCasaMascotas.App.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            AddMascota();
+
+            var Rockysano= new Mascota{
+                 Id=1,
+                 NombreMascota="Rocky111",
+                 Especie="Perro1111",
+                 Raza="Criollo1111",
+                 Edad="14 años1111"
+                };
+            //AddMascota();
+            //BuscarMascota(1);
+            //BorrarMascota(2);
+            //ActualizarMascota(Rockysano);
+
         }
         private static void AddMascota()
         {
@@ -24,6 +36,23 @@ namespace HospiEnCasaMascotas.App.Consola
 
              };
              _repoMascota.AddMascota(mascota);
+        }
+        private static void BuscarMascota(int idMascota){
+           var Mascota= _repoMascota.GetMascota(idMascota);
+           Console.WriteLine("Paciente encontrado"+Mascota.NombreMascota+" "+Mascota.Raza);
+        }
+
+        private static void BorrarMascota(int idMascota){
+            _repoMascota.DeleteMascota(idMascota);
+            Console.WriteLine("La mascota fue borrada");
+        }
+             
+           
+        private static void ActualizarMascota(Mascota Rockysano){
+             
+                var Mascotaactualizada=_repoMascota.UpdateMascota(Rockysano);
+                Console.WriteLine("La mascota se actualizo");
+            
         }
 
     }
