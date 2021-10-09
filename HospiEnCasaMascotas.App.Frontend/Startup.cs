3c9d64ccf95914eeeea84c6edf97114bbe69d620
Andreslis19 <gaml_16@hotmail.com>
2021-10-09 07:31:43 -0500

    asignacion servicios
    proceso asignacion servicio prueba

--- HospiEnCasaMascotas.App.Frontend/Startup.cs
+++ HospiEnCasaMascotas.App.Frontend/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HospiEnCasaMascotas.App.Persistencia;
+using HospiEnCasaMascotas.App.Dominio;
 
 namespace HospiEnCasaMascotas.App.Frontend
 {
@@ -24,6 +26,9 @@ namespace HospiEnCasaMascotas.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<iRepositorioMascota,MascotaRepositorio>();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
