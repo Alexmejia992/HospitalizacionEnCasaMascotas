a5c48c178656abe7568aa3ed9b401d9062ae7a0f
henry2080 <rctbpublicidad@gmail.com>
2021-10-10 19:48:02 -0500

    Registro de Signos Vitales
    Registro de signos vitales dentro del archivo en el modelo .cs

--- HospiEnCasaMascotas.App.Frontend/Pages/RegistroSignosVitales/RegistroSignosVitales.cshtml.cs
+++ HospiEnCasaMascotas.App.Frontend/Pages/RegistroSignosVitales/RegistroSignosVitales.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasaMascotas.App.Persistencia;
+using HospiEnCasaMascotas.App.Dominio;
 
-namespace HospiEnCasaMascostas.App.Fontend
+namespace HospiEnCasaMascostas.App.Fontend.Pages
 {
     public class RegistroSignosVitalesModel : PageModel
     {
+        private readonly iRepositorioSignoVital _RepoSignoVital;
+        public SignoVital signovital{get; set;}
+
+        public RegistroSignosVitalesModel(iRepositorioSignoVital _RepoSignoVital)
+        {
+        this._RepoSignoVital = _RepoSignoVital;
+        }
         public void OnGet()
         {
+            signovital = new SignoVital();
         }
+        
+        _RepoSignoVital.AddSignoVital(signovital);
+        return Page();
     }
 }
