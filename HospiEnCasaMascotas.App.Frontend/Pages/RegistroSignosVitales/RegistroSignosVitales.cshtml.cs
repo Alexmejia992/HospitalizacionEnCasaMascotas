cccc064c261ac3b0ada8438767417028dd3d8412
Andreslis19 <gaml_16@hotmail.com>
2021-10-11 22:20:26 -0500

    Revert "Registro de Signos Vitales"
    This reverts commit a5c48c178656abe7568aa3ed9b401d9062ae7a0f.

--- HospiEnCasaMascotas.App.Frontend/Pages/RegistroSignosVitales/RegistroSignosVitales.cshtml.cs
+++ HospiEnCasaMascotas.App.Frontend/Pages/RegistroSignosVitales/RegistroSignosVitales.cshtml.cs
@@ -4,26 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using HospiEnCasaMascotas.App.Persistencia;
-using HospiEnCasaMascotas.App.Dominio;
 
-namespace HospiEnCasaMascostas.App.Fontend.Pages
+namespace HospiEnCasaMascostas.App.Fontend
 {
     public class RegistroSignosVitalesModel : PageModel
     {
-        private readonly iRepositorioSignoVital _RepoSignoVital;
-        public SignoVital signovital{get; set;}
-
-        public RegistroSignosVitalesModel(iRepositorioSignoVital _RepoSignoVital)
-        {
-        this._RepoSignoVital = _RepoSignoVital;
-        }
         public void OnGet()
         {
-            signovital = new SignoVital();
         }
-        
-        _RepoSignoVital.AddSignoVital(signovital);
-        return Page();
     }
 }
