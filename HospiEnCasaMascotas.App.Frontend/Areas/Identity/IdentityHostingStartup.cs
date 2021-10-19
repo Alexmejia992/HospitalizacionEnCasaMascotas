4c5e1a18e862533dc39252cf640c3843a547dfbc
Alexmejia992 <alexmejia992@gmail.com>
2021-10-19 13:24:10 -0500

    add login identity frontend

--- HospiEnCasaMascotas.App.Frontend/Areas/Identity/IdentityHostingStartup.cs
+++ HospiEnCasaMascotas.App.Frontend/Areas/Identity/IdentityHostingStartup.cs
@@ -19,7 +19,7 @@ namespace HospiEnCasaMascotas.App.Frontend.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("IdentityDataContextConnection")));
 
-                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                     .AddEntityFrameworkStores<IdentityDataContext>();
             });
         }
