dfb94eb5e1b45cdf2c5199601c35ae3284e911d2
henry2080 <rctbpublicidad@gmail.com>
2021-11-01 22:52:39 -0500

    Razor Signos Vitales
    Modelo y Vista en Proceso

--- HospiEnCasaMascotas.App.Frontend/RegistroSignosVitales.cshtml.cs
+++ HospiEnCasaMascotas.App.Frontend/RegistroSignosVitales.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasaMascotas.App.Persistencia;
+using HospiEnCasaMascotas.App.Dominio;
 
-namespace \HospiEnCasaMascotas.App.Frontend\Vet
+namespace HospiEnCasaMascotas.App.Frontend.Vet
 {
     public class RegistroSignosVitalesModel : PageModel
-    {
-        public void OnGet()
         {
+        
+        private readonly iRepositorioSignoVital _RepoSignoVital;
+
+        public SignoVital signovital { get; set; }
+        
+
+            public  RegistroSignosVitalesModel (iRepositorioSignoVital _RepoSignoVital)
+            {
+                this._RepoSignoVital=_RepoSignoVital;
+            }
+
+            public void OnGet()
+            {
+                signovital= new SignoVital();
+            }
+            public IActionResult OnPost(SignoVital signovital)
+            {
+                _RepoVeterinario.AddVeterinario(veterinario);
+             return RedirectToPage("/Vet/RegistroSignosVitales");
+
+            }
+    
         }
+        
     }
-}
